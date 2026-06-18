2a68f29cfedffe7592adea7fc805062077b7ec46
Rodrigo Vieira <tstrodrigovieira@gmail.com>
2026-06-17 22:29:51 -0300

    Fix closing tag for aside element in App.tsx

--- App.tsx
+++ App.tsx
@@ -172,7 +172,7 @@ const App: React.FC = () => {
         <button onClick={() => signOut(auth)} className="p-8 border-t border-slate-100 flex items-center gap-2 text-slate-400 font-bold text-xs uppercase tracking-widest hover:text-rose-600 transition-colors">
           <LogOut size={18}/> Sair
         </button>
-      </</aside>
+      </aside>
 
       <main className="flex-1 flex flex-col min-w-0 overflow-hidden">
         {notification && (
